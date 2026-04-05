2896d8dc7253102c3325a6d18eaf53f260aa25c7
muhammadatef-web <57808343+muhammadatef-web@users.noreply.github.com>
2026-04-05 17:09:53 +0200

    the last version of basketball
    the last version of basketball dashboard

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
-document.getElementById("home")
+let homecount = 0
+document.getElementById("home-content").textContent = homecount
+document.getElementById("home-plus1")
+document.getElementById("home-plus2")
+document.getElementById("home-plus3")
+
+function clickinghomeplus1() {
+       homecount+=1
+       document.getElementById("home-content").textContent = homecount
+
+
+}
+function clickinghomeplus2() {  
+         homecount+=2
+            document.getElementById("home-content").textContent = homecount
+}
+
+function clickinghomeplus3() {
+         homecount+=3
+            document.getElementById("home-content").textContent = homecount
+}
+
+let guestcount = 0
+document.getElementById("guest-content").textContent = guestcount
+document.getElementById("guest-plus1")
+document.getElementById("guest-plus2")
+document.getElementById("guest-plus3")
+
+
+function clickingguestplus1() {
+       guestcount+=1
+       document.getElementById("guest-content").textContent = guestcount
+
+}
+function clickingguestplus2() {
+       guestcount+=2
+       document.getElementById("guest-content").textContent = guestcount
+}
+
+function clickingguestplus3() {
+       guestcount+=3
+       document.getElementById("guest-content").textContent = guestcount
+}               
+
+
